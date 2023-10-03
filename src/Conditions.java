e52534c73559a1519e7994e08c748a05ab863304
anissiam <anis.siam@hotmail.com>
2023-10-03 16:54:35 +0300

    //If

--- src/Conditions.java
+++ src/Conditions.java
@@ -1,10 +1,121 @@
+import javax.sound.sampled.spi.MixerProvider;
 import java.util.Scanner;
 
 public class Conditions {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Plz enter the num");
+        /*System.out.println("Plz enter the mark");
+        byte mark = input.nextByte();
+
+        if (mark>100 || mark<0){
+            System.out.println("Not valid");
+            return;
+        }
+
+        if (mark>=90){
+            System.out.println("NERD");
+        }else if(mark>=80){
+            System.out.println("Vary good");
+        }else if (mark>=70) {
+            System.out.println("Good");
+        }else if (mark>=60){
+            System.out.println("Passed");
+        }else {
+            System.out.println("روح ع الورشة");
+        }
+*/
+
+
+        /*System.out.println("Plz enter the num");
+        int num = input.nextInt();
+
+        if (num==0){
+            System.out.println("Zero");
+        } else if (num % 2 != 0) {
+            System.out.println("Odd");
+        }else {
+            System.out.println("Even");
+            if (num>0){
+                System.out.println("+");
+            }else {
+                System.out.println("-");
+            }
+        }*/
+
+        /*System.out.println("plz enter the full name");
+        String fullName = input.nextLine();
+
+        System.out.println("Plz enter your age");
+        int age = input.nextInt();
+
+        if (fullName.isEmpty()){
+            System.out.println("Empty name");
+            return;
+        }
+
+        if (age == 0) {
+            System.out.println("Empty age");
+            return;
+        }
+
+
+
+        if (age>=19){
+            input.nextLine();
+            System.out.println("Plz enter the major");
+            String major = input.nextLine();
+
+            System.out.println("Name " + fullName + " Age:" + age + " Major:" + major);
+        }else {
+            input.nextLine();
+
+            System.out.println("Enter your grade");
+            String grade = input.nextLine();
+
+            System.out.println("Name " + fullName + " Age:" + age + " Grade:" + grade);
+
+
+        }*/
+        /*if (fullName.isEmpty()){
+            System.out.println("Empty full name");
+            return;
+        }*/
+
+
+        /*System.out.println("plz enter the last name");
+        String lastName = input.nextLine().toLowerCase();*/
+
+        /*if (lastName.isEmpty()){
+            System.out.println("Empty last name");
+            return;
+        }*/
+
+        /*if (!fullName.isEmpty() && !lastName.isEmpty()){
+            if (fullName.contains(lastName)) {
+                System.out.println("Exist");
+            }else {
+                System.out.println("Not exist");
+            }
+        }else {
+            System.out.println("Empty");
+        }
+*/
+
+
+
+
+
+
+        /*char c = input.nextLine().charAt(0);
+
+        if (c == 'a' || c=='A') {
+            System.out.println("Start with A");
+        }*/
+
+
+
+        /*System.out.println("Plz enter the num");
         int num = input.nextInt();
 
         if (num==0){
@@ -16,7 +127,7 @@ public class Conditions {
             System.out.println("Even");
         }else {
             System.out.println("Odd");
-        }
+        }*/
 
 
         /*System.out.println("Plz enter the mark");
--- src/InputData.java
+++ src/InputData.java
@@ -3,6 +3,49 @@ import java.util.Scanner;
 public class InputData {
     public static void main(String[] args) {
 
+
+        int x = 2022;
+
+        System.out.println();
+        System.out.println(x / 100);
+        System.out.println(x / 1000);
+        System.out.println(x / 1000);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("n1");
+        int n1 = scanner.nextInt();
+
+        System.out.println("n2");
+        int n2 = scanner.nextInt();
+
+        System.out.println("n3");
+        int n3 = scanner.nextInt();
+
+        System.out.println("n4");
+        int n4 = scanner.nextInt();
+
+
+        int ava = (n1 + n2 + n3 + n4) / 4;
+        System.out.println(ava);
+
+        /*System.out.println("plz enter first santence");
+        String s1 = scanner.nextLine();
+
+        System.out.println("plz enter sec santence");
+        String s2 = scanner.nextLine();
+
+        int l = s1.concat(s2).length();
+        System.out.println(l);*/
+
+        //read , in , input , scanner
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Plz enter the year");
+        String y = scanner.nextLine();
+
+        System.out.println(y.charAt(0));
+        System.out.println(y.charAt(1));
+        System.out.println(y.charAt(2));
+        System.out.println(y.charAt(3));*/
+
       /*  Scanner input = new Scanner(System.in);//instance/object from Scanner named input
         System.out.println("Please enter your age");
         int age = input.nextInt();
